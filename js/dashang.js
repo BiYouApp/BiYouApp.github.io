1411ebb7f4b88d1e66cc11305e6d74bf19ef8a22
LiuJieTan <1780973645@qq.com>
2019-12-12 21:28:20 +0800

    img

--- js/dashang.js
+++ js/dashang.js
@@ -2,11 +2,11 @@
 function checkWeChat(){
     $("input[name='pay-type']").get(0).checked=true;
     $("#pay-type-title").html('微信');
-    $("#pay-image").attr("src","images/pay/wechat.png");
+    $("#pay-image").attr("src","https://liujietan.gitee.io/biyou/img/pay/wechat.png");
 }
 
 function checkAliPay(){
     $("input[name='pay-type']").get(1).checked=true;
     $("#pay-type-title").html('支付宝');
-    $("#pay-image").attr("src","images/pay/alipay.png");
+    $("#pay-image").attr("src","https://liujietan.gitee.io/biyou/img/pay/alipay.png");
 }
